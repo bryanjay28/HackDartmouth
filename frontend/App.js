e31388c09369dbf29c3429aa006cce61d21e1c61
bryanjay28 <bjay28@hotmail.com>
2019-04-06 19:04:46 -0400

    he

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Expo, Permissions, ImagePicker } from 'expo';
-
-// import CameraScreen from './components/Camera';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { Expo, Audio, Permissions, ImagePicker } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.recording = null;
     this.state = {
       welcomeScreen: true,
       hasCameraPermission: false,
       hasMicrophonePermission: false,
       startRecording: false,
+      notRecording: true,
     }
   }
 
@@ -24,15 +24,18 @@ export default class App extends React.Component {
     this.setState({ hasMicrophonePermission: microphonePermissions.status === 'granted' });
   }
 
-  _askForPermissions = async () => {
-    const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    this.setState({
-      haveRecordingPermissions: response.status === 'granted',
-    });
-  };
+  componentDidUpdate = () => {
+    const { startRecording, notRecording } = this.state;
+    if (startRecording && notRecording) {
+      this._recordSound();
+    }
+  }
 
   render() {
-    const { welcomeScreen, hasCameraPermission, hasMicrophonePermission } = this.state;
+    const { welcomeScreen,
+      hasCameraPermission,
+      hasMicrophonePermission,
+    } = this.state;
     if (!hasCameraPermission || !hasMicrophonePermission) {
       return (
         <Text>Please give camera or microphone access</Text>
@@ -48,7 +51,6 @@ export default class App extends React.Component {
             onPress={() => {
               this.setState({
                 welcomeScreen: false,
-                startRecording: true
               })
             }} />
         </View>
@@ -57,8 +59,28 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           <Text>CAMERA SHOULD BE HERE + THERE WILL BE SOUND</Text>
+          <Button title="Record" />
         </View>
       );
+    }
+  }
+
+  _recordSound = async () => {
+    console.log('start recording');
+    const { notRecording } = this.state;
+    this.setState({ notRecording: !notRecording });
+
+    const recording = new Audio.Recording();
+    try {
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync();
+      // recording now
+      this.recording = recording;
+      console.log(recording);
+
+    } catch (error) {
+      console.log('Error with Recording');
+      console.log(error);
     }
   }
 }
